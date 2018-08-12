540ed2f1a65fb4e497ad6989c911506c014cf9d6
lucastobrazil <lucas.arundell@gmail.com>
2018-08-12 22:34:38 +0200

    rmove more css

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.less';
 
 class App extends Component {
@@ -7,10 +6,7 @@ class App extends Component {
 
     return (
       <div className={styles.app}>
-        <div className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h2>Ready to get started</h2>
-        </div>
+          <h2>Ready to get started!</h2>
       </div>
     );
   }
